61b4c7671cdc16b414bba5c8e7ef7966225fce9c
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2021-01-29 21:08:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def main():
 	if uploaded_file is not None:
 		file_bytes=np.asarray(bytearray(uploaded_file.read()),dtype=np.uint8)
 		opencv_image=cv2.imdecode(file_bytes,1)
-		st.image(opencv_image,channels="BGR"
+		st.image(opencv_image,channels="BGR")
 		imagee(opencv_image)    		
 
 
