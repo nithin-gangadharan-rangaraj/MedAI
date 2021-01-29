116f6c175df5db4020787170763fe44964e1b933
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2021-01-29 20:54:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,6 @@ def main():
     		# Convert the file to an opencv image.
     		file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
     		opencv_image = cv2.imdecode(file_bytes, 1)
-
     		# Now do something with the image! For example, let's display it:
     		st.image(opencv_image, channels="BGR")
 		imagee(opencv_image)
