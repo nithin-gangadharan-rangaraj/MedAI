69bf6165c4682b8cbfbe7b279e9383992faa69d6
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2021-01-29 19:13:26 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import cv2
+import numpy as np
+
+PAGE_CONFIG = {"page_title":"Arsya.io","page_icon":":smiley:","layout":"centered"}
+st.set_page_config(**PAGE_CONFIG)
+
+html = '''
+<style>
+body {
+background-image: url("https://img.freepik.com/free-vector/white-elegant-texture-wallpaper_23-2148421854.jpg?size=626&ext=jpg&ga=GA1.2.145878890.1611360000");
+background-size: cover;
+}
+</style>
+'''
+
+
+def main():
+	st.title("Hello Sannamyr!")
+	st.markdown(html, unsafe_allow_html=True)
+	uploaded_file = st.file_uploader("Choose a image file", type="jpg")
+	if uploaded_file is not None:
+    		# Convert the file to an opencv image.
+    		file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+    		opencv_image = cv2.imdecode(file_bytes, 1)
+
+    		# Now do something with the image! For example, let's display it:
+    		st.image(opencv_image, channels="BGR")
+
+if __name__ == '__main__':
+	main()
